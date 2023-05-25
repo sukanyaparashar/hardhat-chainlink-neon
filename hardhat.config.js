db792ef378dfd1fa0810f171ba4f8e26d133ba4a
0xLucian <0xluciandev@gmail.com>
2023-05-25 18:33:33 +0300

    add etherscan verify dependency

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
     artifacts: "./artifacts",
   },
   networks: {
-    neonlabs: {
+    neondevnet: {
       url: NEON_PROXY_URL,
       accounts: [NEON_ACCOUNTS],
       chainId: 245022926,
@@ -18,4 +18,19 @@ module.exports = {
       isFork: true,
     },
   },
+  etherscan: {
+  apiKey: {
+    neondevnet: "test"
+  },
+  customChains: [
+    {
+      network: "neondevnet",
+      chainId: 245022926,
+      urls: {
+        apiURL: "https://devnet-api.neonscan.org/hardhat/verify",
+        browserURL: "https://devnet.neonscan.org"
+      }
+    }
+  ]
+}
 };
